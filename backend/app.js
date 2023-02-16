15a47714f1056792d39b602eed9fdd56a3c4bdcf
sergio.marin <sergio.marin@durango.gob.mx>
2023-02-16 16:55:50 -0600

    feat: :sparkles: Configurar el acceso CORS en NodeJS

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,17 @@ var projectRoutes = require('./routes/project');
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());//Para convertir la data que llege en un json
 
+
+
 //CORS
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //RUTAS
 app.use('/api', projectRoutes)
