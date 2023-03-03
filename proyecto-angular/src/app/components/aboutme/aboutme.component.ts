38ca2298ed84ec9eab5df75f31563c0510aa73f4
sergio.marin <sergio.marin@durango.gob.mx>
2023-03-03 14:53:47 -0600

    style: :lipstick: Estilos/Pagina sobre mi

--- proyecto-angular/src/app/components/aboutme/aboutme.component.ts
+++ proyecto-angular/src/app/components/aboutme/aboutme.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
   styleUrls: ['./aboutme.component.css']
 })
-export class AboutmeComponent {
+export class AboutmeComponent implements OnInit {
+  public title: String;
+  public subtitle: String;
+  public email: String;
+
+  constructor(){
+    this.title = "Sergio Alejandro Marin Sarmiento"
+    this.subtitle = "Desarrollador Junior Advanced Web"
+    this.email = "correo@correo.com"
+
+  }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+  }
 
 }
