3e935e8ffa22a131286d8a89ca83531cb19c3f38
sergio.marin <sergio.marin@durango.gob.mx>
2023-05-03 14:55:42 -0600

    feat(FullStack): :sparkles: Implementacion de Jquery en Angular

--- proyecto-angular/src/app/components/contact/contact.component.ts
+++ proyecto-angular/src/app/components/contact/contact.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    $("#logo").click(function (e) {
+      e.preventDefault();
+      $("header")
+          .css("background", "green")
+          .css("height", "50px");
+
+    });
+  }
 
 }
