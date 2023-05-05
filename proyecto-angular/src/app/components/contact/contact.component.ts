fe4594ce4e6a81b1d126b5b4335a26b56b4fb016
sergio.marin <sergio.marin@durango.gob.mx>
2023-05-05 12:20:31 -0600

    feat(FullStack): :sparkles: Propiedad Output - De hijos a padres y ViewChilds

--- proyecto-angular/src/app/components/contact/contact.component.ts
+++ proyecto-angular/src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit,ViewChild } from '@angular/core';
 declare const $:any;
 
 @Component({
@@ -8,14 +8,23 @@ declare const $:any;
 })
 export class ContactComponent implements OnInit {
   public widthSlider : number = 0;
-  public anchuraToSlider : number = 0;
+  public anchuraToSlider : any = 0;
   public captions : boolean;
+  public autor: any;
+
+  //ViewChild
+  @ViewChild('textos',{static:true}) textos?:ElementRef;
 
   constructor(){
     this.captions = true;
   }
 
   ngOnInit(){
+    let opcion_clasica = document.querySelector('#texto')?.innerHTML; //Este la opcion de JS que busca la info con el viewchild
+    console.log("1 ----> "+opcion_clasica);
+    console.log("2 ----> ",this.textos);
+    console.log("3 ----> ",this.textos?.nativeElement.textContent);
+
   }
 
   cargarSlider(){
@@ -23,7 +32,13 @@ export class ContactComponent implements OnInit {
   }
 
   resetearSlider(){
-    this.anchuraToSlider = 0;
+    this.anchuraToSlider = false;
   }
 
+  //Con el evenmitter nos trae el evento con el json de la clase Hijo
+  getAutor(event:any){
+    console.log(event);
+    this.autor = event;
+
+  }
 }
